73bbe1d70b20a1e7300b8e0ccad82ead47cce54a
Hỷ Nguyễn <hy@misfit.com>
2016-04-24 13:09:20 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -19,11 +19,21 @@ mkdir classes
 #curl -L -o mail-1.4.7.jar http://central.maven.org/maven2/javax/mail/mail/1.4.7/mail-1.4.7.jar
 #cd ..
 
+# clean old compile
+rm -rf ./classes/*
+rm dist/JMail.jar
+# compile code
 javac -d ./classes -classpath ./libs/mail-1.4.7.jar:./libs/commons-io-2.5.jar ./src/jmail/JMail.java 
+
 
 cd ./classes
 jar cfm ../dist/JMail.jar ../Manifest.txt ./jmail/*
 cd ..
 
-echo 'java -jar dist/JMail.jar "<Email Subject>" "<email form>" "<email form password>" "<email to>" "<message file or message string>" ["<Path to file attachments>" [...] ]'
+# read file message or attach files from current dir of dist/JMail.jar file
+echo 'Please run as 2 ways:'
+echo '1: java -jar dist/JMail.jar "<Email Subject>" "<email form>" "<email form password>" "<email to>" "<message file or message string>" ["<Path to file attachments>" [...] ]'
+
+# read file message or attach files from WORK_DIR enviroment
+echo '2: WORK_DIR=`pwd` java -jar dist/JMail.jar "<Email Subject>" "<email form>" "<email form password>" "<email to>" "<message file or message string>" ["<Path to file attachments>" [...] ]'
 
